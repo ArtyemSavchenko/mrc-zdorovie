1ae2177ed63e41aa8cbee764d9fdb2331fb8c390
Artyem Savchenko <a.savchenko89@yandex.ru>
2022-08-18 18:53:31 +0300

    refactor: Перенес скрипт отправки формы теперь сам скрипт лежит в соотвествующем странице файле js, внешний скрипт скачан и лежит в vendor

--- pages/registration.js
+++ pages/registration.js
@@ -1,1 +1,29 @@
 import '../utils/mobile-menu.js';
+import '../vendor/email.min.js';
+
+(function () {
+  emailjs.init('user_dOgOzdD73w4Q1jEouOYa2');
+})();
+
+window.onload = function () {
+  const form = document.getElementById('contact-form');
+
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+
+    this.contact_number.value = Math.random() * 100000 | 0;
+
+    emailjs.sendForm('service_5vaqoms', 'template_8n9hs3f', this)
+      .then(() => {
+        alert('Запрос выполнен успешно.');
+        console.log('SUCCESS!');
+      })
+
+      .catch((err) => {
+        alert('Что-то пошло не так.');
+        console.log('FAILED...', err);
+      });
+
+    form.reset();
+  });
+}
