69e9695dca8f45ef9844b2401905b3bfd20823e0
Artyem Savchenko <a.savchenko89@yandex.ru>
2023-10-22 13:34:25 +0300

    refactor: Правки текстов

--- utils/api-params.js
+++ utils/api-params.js
@@ -1,6 +1,6 @@
 export const apiParams = {
-  serviceId: 'service_7g2ou2j',
-  templateId: 'template_2z5ltxc',
-  userId: 'he37NdbQEPcTkrowP', //publicKey
+  serviceId: 'service_5vaqoms',
+  templateId: 'template_n5205yv',
+  userId: 'user_dOgOzdD73w4Q1jEouOYa2', //publicKey
   apiUrl: 'https://api.emailjs.com/api/v1.0/email/send',
-};
+}
